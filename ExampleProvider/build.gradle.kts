df8942c1bdbf696ebcfcc503fbd186e2124471a1
Dangerous-Taabar <vishalmeena968059@gmail.com>
2026-09-11 22:57:08 +0530

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -3,20 +3,12 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
 
-// Use an integer for version numbers
 version = 1
 
 cloudstream {
     description = "Desi TV Serialz - Hindi TV Shows Extension"
     authors = listOf("Taabar")
 
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
     status = 1
 
     tvTypes = listOf("TvSeries", "Movie")
